661b4c4c2327619140254ccb222665d46e8a532f
Мунтян Максим <maksimuntyan@gmail.com>
2025-02-18 23:58:10 +0200

    com

---
+++ WindowsProject4/WindowsProject4.cpp
@@ -0,0 +1,67 @@
+﻿#include <windows.h>
+#include <tchar.h>
+#include "resource.h"
+
+BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+void CreateStaticControl(HWND, int, int, int, int);
+
+POINT startPoint;
+bool isDragging = false;
+int staticCounter = 0;
+
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, int nCmdShow)
+{
+    return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc);
+}
+
+BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_LBUTTONDOWN:
+        startPoint.x = LOWORD(lParam);
+        startPoint.y = HIWORD(lParam);
+        isDragging = true;
+        return TRUE;
+
+    case WM_LBUTTONUP:
+        if (isDragging)
+        {
+            int x1 = startPoint.x;
+            int y1 = startPoint.y;
+            int x2 = LOWORD(lParam);
+            int y2 = HIWORD(lParam);
+
+            int left = min(x1, x2);
+            int top = min(y1, y2);
+            int width = max(10, abs(x2 - x1));
+            int height = max(10, abs(y2 - y1));
+
+            if (width == 10 || height == 10)
+            {
+                MessageBox(hWnd, _T("Слишком маленькая область!"), _T("Внимание"), MB_OK | MB_ICONWARNING);
+            }
+            else
+            {
+                CreateStaticControl(hWnd, left, top, width, height);
+            }
+
+            isDragging = false;
+        }
+        return TRUE;
+
+    case WM_CLOSE:
+        EndDialog(hWnd, 0);
+        return TRUE;
+    }
+    return FALSE;
+}
+
+void CreateStaticControl(HWND hWnd, int x, int y, int width, int height)
+{
+    TCHAR textBuffer[10];
+    wsprintf(textBuffer, _T("%d"), staticCounter++);
+
+    CreateWindow(_T("STATIC"), textBuffer, WS_CHILD | WS_VISIBLE | SS_CENTER | SS_SUNKEN,
+        x, y, width, height, hWnd, NULL, GetModuleHandle(NULL), NULL);
+}
