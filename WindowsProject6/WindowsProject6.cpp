3c6fe7d845f6ca3db65165d9c8f5cbce09ad6a42
Munty_sx88 <Munty_sx88@28-9>
2025-02-20 11:14:26 +0200

    com

---
+++ WindowsProject6/WindowsProject6.cpp
@@ -0,0 +1,29 @@
+#include <windows.h>
+#include <tchar.h>
+#include "resource.h"
+
+BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+
+
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, int nCmdShow)
+{
+
+   
+    return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc);
+}
+
+BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    int score;
+    switch (message)
+    {
+    case WM_COMMAND:
+
+    case WM_SHOWWINDOW:
+        return TRUE;
+    case WM_CLOSE:
+        EndDialog(hWnd, 0);
+        return TRUE;
+    }
+    return FALSE;
+}
