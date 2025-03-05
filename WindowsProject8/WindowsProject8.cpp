3a62d6153d8621db59ed2182fe185f5df7d19602
maksi <maksi@BOB>
2025-03-06 00:24:01 +0200

    fdfdsfdsf

--- WindowsProject8/WindowsProject8.cpp
+++ WindowsProject8/WindowsProject8.cpp
@@ -1,29 +1,90 @@
 #include <windows.h>
-#include <tchar.h>
+#include <fstream>
 #include "resource.h"
+#include <commctrl.h>
 
-BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+using namespace std;
 
+INT_PTR CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+DWORD WINAPI CopyFileThread(LPVOID lpParam);
 
-int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, int nCmdShow)
+struct CopyData {
+    HWND hWnd;
+    char sourcePath[MAX_PATH];
+    char pastePath[MAX_PATH];
+};
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-
-    // создаём главное окно приложения на основе модального диалога
-    return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc);
+    return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc);
 }
 
-BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+void StartCopying(HWND hWnd)
 {
-    int score;
+    CopyData* data = new CopyData;
+    data->hWnd = hWnd;
+
+    GetDlgItemTextA(hWnd, IDC_EDIT1, data->sourcePath, MAX_PATH);
+    GetDlgItemTextA(hWnd, IDC_EDIT2, data->pastePath, MAX_PATH);
+
+    CreateThread(NULL, 0, CopyFileThread, data, 0, NULL);
+}
+
+DWORD WINAPI CopyFileThread(LPVOID lpParam)
+{
+    CopyData* data = (CopyData*)lpParam;
+
+    ifstream src(data->sourcePath, ios::binary);
+    ofstream paste(data->pastePath, ios::binary);
+
+    if (src && paste) {
+        src.seekg(0, ios::end);
+        long fileSize = src.tellg();
+        src.seekg(0, ios::beg);
+
+        char ch;
+        long copiedSize = 0;
+        HWND hProgress = GetDlgItem(data->hWnd, IDC_PROGRESS1);
+        SendMessage(hProgress, PBM_SETRANGE, 0, MAKELPARAM(0, 100));
+
+        while (src.get(ch)) {
+            Sleep(10);
+            paste.put(ch);
+            copiedSize++;
+            if (fileSize > 0) { 
+                int progress = (copiedSize * 100) / fileSize;
+                SendMessage(hProgress, PBM_SETPOS, progress, 0);
+            }
+        }
+        MessageBoxA(data->hWnd, "Скопировано", "Гуд", MB_OK | MB_ICONINFORMATION);
+    }
+    else {
+        MessageBoxA(data->hWnd, "Ошибка", "Ошибка", MB_OK | MB_ICONERROR);
+    }
+
+    delete data;
+    return 0;
+}
+
+INT_PTR CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
     switch (message)
     {
-    case WM_COMMAND:
-
-    case WM_SHOWWINDOW:
-        return TRUE;
     case WM_CLOSE:
         EndDialog(hWnd, 0);
         return TRUE;
+
+    case WM_INITDIALOG:
+        SetDlgItemTextA(hWnd, IDC_EDIT1, "C:\\Users\\maksi\\OneDrive\\Рабочий стол\\example.txt");
+        SetDlgItemTextA(hWnd, IDC_EDIT2, "C:\\Users\\maksi\\OneDrive\\Изображения\\copy.txt");
+        return TRUE;
+
+    case WM_COMMAND:
+        if (wParam == IDC_BUTTON2) {
+            StartCopying(hWnd);
+            return TRUE;
+        }
+        break;
     }
     return FALSE;
 }
